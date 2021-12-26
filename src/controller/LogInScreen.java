c2ba919f6e864bc3072c06b565d0c3f5429bb522
Vadym Feskin <Vadym.feskin@gmail.com>
2021-12-26 11:48:14 -0800

    implemented user log in validation

--- src/controller/LogInScreen.java
+++ src/controller/LogInScreen.java
@@ -2,14 +2,15 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import java.io.IOException;
+import java.net.URL;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ResourceBundle;
 
-public class LogInScreen extends SuperController {
+public class LogInScreen extends SuperController implements Initializable {
 
     @FXML
     private Label timezoneLabel;
@@ -25,7 +26,45 @@ public class LogInScreen extends SuperController {
 
     @FXML
     void onActionDisplayMainScreen(ActionEvent event) throws IOException {
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        String userName = null; // sqlUser
+        String password = null; // Passw0rd!
+
+        // username input verification
+        try {
+            userName = userNameText.getText();
+            if(userName == null || userName.isBlank() || !userName.equals("sqlUser") ) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            errorAlert.setHeaderText("Incorrect user name");
+            errorAlert.setContentText("Please try again");
+            errorAlert.showAndWait();
+            return;
+        }
+
+        // password input verification
+        try {
+            password = passwordText.getText();
+            if(password == null || password.isBlank() || !password.equals("Passw0rd!") ) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            errorAlert.setHeaderText("Incorrect password");
+            errorAlert.setContentText("Please try again");
+            errorAlert.showAndWait();
+            return;
+        }
+
         displayNewScreen(logInButton, "/view/Appointments.fxml");
     }
 
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ZoneId zone = ZoneId.systemDefault();
+        String zoneDisplay = zone.getId();
+        timezoneLabel.setText(zoneDisplay);
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
-        //stage.setTitle("Main Screen");
+//        Parent root = FXMLLoader.load(getClass().getResource("/view/Appointments.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
     }
