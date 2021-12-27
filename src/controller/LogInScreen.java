d8c4434fbf03122f2d8c62b7010d528936c31c86
Vadym Feskin <Vadym.feskin@gmail.com>
2021-12-27 12:13:08 -0800

    worked on implementing user log in attempts

--- src/controller/LogInScreen.java
+++ src/controller/LogInScreen.java
@@ -4,10 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -30,39 +35,75 @@ public class LogInScreen extends SuperController implements Initializable {
         Alert errorAlert = new Alert(Alert.AlertType.ERROR);
         String userName = null; // sqlUser
         String password = null; // Passw0rd!
+        boolean logInAttempt = false;
+        int counter = 1;
 
-        // username input verification
+//        try {
+//            userName = userNameText.getText();
+//            if (userName == null || userName.isBlank() || !userName.equals("sqlUser")) {
+//                throw new Exception();
+//            }
+//        } catch (Exception e) {
+//            errorAlert.setHeaderText("Incorrect user name");
+//            errorAlert.setContentText("Please try again");
+//            errorAlert.showAndWait();
+//            return;
+//        }
+//
+//        // password input verification
+//        try {
+//            password = passwordText.getText();
+//            if (password == null || password.isBlank() || !password.equals("Passw0rd!")) {
+//                throw new Exception();
+//            }
+//        } catch (Exception e) {
+//            errorAlert.setHeaderText("Incorrect password");
+//            errorAlert.setContentText("Please try again");
+//            errorAlert.showAndWait();
+//            return;
+//        }
+
         try {
             userName = userNameText.getText();
-            if(userName == null || userName.isBlank() || !userName.equals("sqlUser") ) {
+            password = passwordText.getText();
+
+
+            if ((userName == null || userName.isBlank() || !userName.equals("sqlUser")) ||
+             (password == null || password.isBlank() || !password.equals("Passw0rd!"))) {
+                counter++;
+                userActivity(counter, logInAttempt);
                 throw new Exception();
             }
         } catch (Exception e) {
-            errorAlert.setHeaderText("Incorrect user name");
+            errorAlert.setHeaderText("Incorrect credentials");
             errorAlert.setContentText("Please try again");
             errorAlert.showAndWait();
             return;
         }
 
-        // password input verification
-        try {
-            password = passwordText.getText();
-            if(password == null || password.isBlank() || !password.equals("Passw0rd!") ) {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            errorAlert.setHeaderText("Incorrect password");
-            errorAlert.setContentText("Please try again");
-            errorAlert.showAndWait();
-            return;
-        }
+        logInAttempt = true;
+        userActivity(counter, logInAttempt);
 
         displayNewScreen(logInButton, "/view/Appointments.fxml");
     }
 
+    // user log-in attempts
+    public void userActivity(int count, boolean attempt) throws IOException {
+        LocalDateTime time = LocalDateTime.now();
+
+        // try-with-resources, auto closes
+        try (PrintWriter pw = new PrintWriter(new FileWriter("login_activity.txt"))){
+            pw.println(count);
+            pw.println(time);
+            pw.println(attempt);
+        }
+    }
+
+
+
     public static void main(String[] args) {
-//        // test
-//        Locale.setDefault((new Locale("fr")));
+        // test
+        Locale.setDefault((new Locale("fr")));
 
         ResourceBundle resourceBundle = ResourceBundle.getBundle("/utilities/Localization_fr.properties",
                 Locale.getDefault());
